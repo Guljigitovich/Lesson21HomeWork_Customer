d8e8a3da22c1f42ffa484dc99b1e81c4984867c0
Saparov Syimyk <guljigitovich.180698@gmai.com>
2022-11-30 17:26:27 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -12,21 +12,23 @@ public class Main {
                 new Customer("Mustafa","Ahmet",Country.TURKEY, LocalDate.of(1994,8,24), "tr.ahmet_43"),
                 new Customer("Aleksei","Sergei",Country.RUSSIA,LocalDate.of(1998,10,16),"aleksei_34"),
         };
-        try {
-            Scanner scanner = new Scanner(System.in);
-            Scanner scanner1 = new Scanner(System.in);
-            for (Customer customer : customers) {
-                switch (scanner1.nextInt()) {
-                    case 1 -> customer.getOfAge(customers);
-                    case 2 -> customer.similarCountries(scanner.nextLine(), customers);
-                    case 3 -> customer.getAllCustomer(customers);
-                    case 4 -> customer.sortCountries(customers);
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                Scanner scanner1 = new Scanner(System.in);
+                for (Customer customer : customers) {
+                    switch (scanner1.nextInt()) {
+                        case 1 -> customer.getOfAge(customers);
+                        case 2 -> customer.similarCountries(scanner.nextLine(), customers);
+                        case 3 -> customer.getAllCustomer(customers);
+                        case 4 -> customer.sortCountries(customers);
+                    }
                 }
+            } catch (InputMismatchException e) {
+                System.out.println("сен сан жазчуу жерге соз жазып койдун");
+            } catch (Exception e) {
+                System.err.println("сен соз жазчуу жерге сан жазып койдун");
             }
-        }catch (InputMismatchException e){
-            System.out.println("сен сан жазчуу жерге соз жазып койдун");
-        }catch (Exception e){
-            System.err.println("сен соз жазчуу жерге сан жазып койдун");
         }
 
 
